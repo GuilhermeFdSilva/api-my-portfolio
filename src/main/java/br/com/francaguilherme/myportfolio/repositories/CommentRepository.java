0edc9233938b6d720b79a4c4de7438eee379d47e
Guilherme <francaguilherme27@gmail.com>
2024-01-19 20:57:32 -0300

    Query findCommentByProjectId | Documentação

--- src/main/java/br/com/francaguilherme/myportfolio/repositories/CommentRepository.java
+++ src/main/java/br/com/francaguilherme/myportfolio/repositories/CommentRepository.java
@@ -1,8 +1,12 @@
 package br.com.francaguilherme.myportfolio.repositories;
 
 import br.com.francaguilherme.myportfolio.models.Comment;
+import br.com.francaguilherme.myportfolio.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Repositório para entidade {@link Comment}. Este repositório fornece métodos para manipulação e persistência de dados
@@ -10,7 +14,16 @@ import org.springframework.stereotype.Repository;
  *
  * @see Repository
  * @see Comment
+ * @see Query
  */
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    /**
+     * Lista todos os comentários de um projeto({@link Project}).
+     *
+     * @param id ID do projeto buscado.
+     * @return A lista dos comentários do projeto.
+     */
+    @Query("SELECT c FROM Comment c WHERE c.project.id = :id")
+    List<Comment> findCommentByProjectId(Long id);
 }
