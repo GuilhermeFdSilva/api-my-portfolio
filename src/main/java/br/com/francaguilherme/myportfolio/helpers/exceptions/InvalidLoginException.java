8150334acf9e97f14d1312a285114f8d8b0c90ea
Guilherme <francaguilherme27@gmail.com>
2024-01-20 13:01:34 -0300

    Atualização de nome da exception e mensagem

--- src/main/java/br/com/francaguilherme/myportfolio/helpers/exceptions/InvalidLoginException.java
+++ src/main/java/br/com/francaguilherme/myportfolio/helpers/exceptions/InvalidLoginException.java
@@ -3,9 +3,9 @@ package br.com.francaguilherme.myportfolio.helpers.exceptions;
 /**
  * Exceção lançada quando uma senha é considerada inválida.
  */
-public class InvalidPasswordException extends RuntimeException {
+public class InvalidLoginException extends RuntimeException {
     @Override
     public String getMessage() {
-        return "Senha incorreta - " + super.getMessage();
+        return "Credenciais de login inválidas.";
     }
 }
