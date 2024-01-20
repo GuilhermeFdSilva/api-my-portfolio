431bf63bcf5fc134ee87a914109b8e3a685bd629
Guilherme <francaguilherme27@gmail.com>
2024-01-20 18:07:07 -0300

    Classe para tratar Exceptions nas controllers

---
+++ src/main/java/br/com/francaguilherme/myportfolio/controllers/adviceController/ExceptionHandlers.java
@@ -0,0 +1,41 @@
+package br.com.francaguilherme.myportfolio.controllers.adviceController;
+
+import br.com.francaguilherme.myportfolio.helpers.exceptions.EmptyListException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+
+@ControllerAdvice
+public class ExceptionHandlers {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handlerEntityNotFound(EntityNotFoundException exception) {
+        return new ResponseEntity<>("Dados fornecidos inválidos: erro no objeto fornecido.", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(EmptyListException.class)
+    public ResponseEntity<String> handlerEmptyList(EmptyListException exception) {
+        return new ResponseEntity<>("Lista de comentários vazia ou nula", HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handlerArgumentNotValid(MethodArgumentNotValidException exception) {
+        BindingResult result = exception.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+
+        StringBuilder errorMessage = new StringBuilder("Erros de validação: ");
+
+        for (FieldError fieldError : fieldErrors) {
+            errorMessage.append(fieldError.getDefaultMessage()).append("; ");
+        }
+
+        return new ResponseEntity<>(errorMessage.toString(), HttpStatus.BAD_REQUEST);
+    }
+}
