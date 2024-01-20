43c225333bd92fe1c8e269f4f161d5fe76761b52
Guilherme <francaguilherme27@gmail.com>
2024-01-20 12:54:19 -0300

    Query findAdminByLogin

--- src/main/java/br/com/francaguilherme/myportfolio/repositories/AdminRepository.java
+++ src/main/java/br/com/francaguilherme/myportfolio/repositories/AdminRepository.java
@@ -2,7 +2,10 @@ package br.com.francaguilherme.myportfolio.repositories;
 
 import br.com.francaguilherme.myportfolio.models.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * Repositório da entidade {@link Admin}. Este repositório fornece métodos para manipulação e persistência de dados na
@@ -13,4 +16,12 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+    /**
+     * Encontra um Administrador pelo login.
+     *
+     * @param login Login para busca.
+     * @return Um {@link Optional} de {@link Admin}.
+     */
+    @Query("SELECT a FROM Admin a WHERE a.login = :login")
+    Optional<Admin> findAdminByLogin(String login);
 }
