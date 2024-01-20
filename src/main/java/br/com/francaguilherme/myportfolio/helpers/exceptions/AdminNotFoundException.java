3795d431f92c699f9e5e89f2f0597fc6208f9b4e
Guilherme <francaguilherme27@gmail.com>
2024-01-20 12:53:44 -0300

    Remoção de exception

--- src/main/java/br/com/francaguilherme/myportfolio/helpers/exceptions/AdminNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.com.francaguilherme.myportfolio.helpers.exceptions;
-
-public class AdminNotFoundException extends RuntimeException{
-    @Override
-    public String getMessage() {
-        return "Usuário administrador não encontrado - " + super.getMessage();
-    }
-}
