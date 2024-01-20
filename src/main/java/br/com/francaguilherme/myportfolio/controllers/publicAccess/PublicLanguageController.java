40a0b81e31229aa369b708f74af3724b5bc4ea52
Guilherme <francaguilherme27@gmail.com>
2024-01-20 20:19:41 -0300

    Controller publica de linguagens

---
+++ src/main/java/br/com/francaguilherme/myportfolio/controllers/publicAccess/PublicLanguageController.java
@@ -0,0 +1,25 @@
+package br.com.francaguilherme.myportfolio.controllers.publicAccess;
+
+import br.com.francaguilherme.myportfolio.models.Language;
+import br.com.francaguilherme.myportfolio.services.LanguageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/public/languages")
+public class PublicLanguageController {
+    @Autowired
+    private LanguageService service;
+
+    @GetMapping
+    public ResponseEntity<List<Language>> listLanguages() {
+        List<Language> languages = service.listLanguages();
+        return new ResponseEntity<>(languages, HttpStatus.OK);
+    }
+}
