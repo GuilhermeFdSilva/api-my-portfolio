3e4568644cd4455b2712b8a9a74ad6b5722d9e2a
Guilherme <francaguilherme27@gmail.com>
2024-01-23 20:36:00 -0300

    Criação da DTO de Comments

---
+++ src/main/java/br/com/francaguilherme/myportfolio/models/DTOs/CommentDTO.java
@@ -0,0 +1,40 @@
+package br.com.francaguilherme.myportfolio.models.DTOs;
+
+import br.com.francaguilherme.myportfolio.models.entities.Comment;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.lang.NonNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CommentDTO {
+    private Long id;
+
+    @NotNull(message = "Não pode ser nulo")
+    @NotBlank(message = "Não pode estar em branco")
+    private String nick;
+
+    @NotNull(message = "Não pode ser nulo")
+    @NotBlank(message = "Não pode estar em branco")
+    private String message;
+
+    @NotNull(message = "Não pode ser nulo")
+    private Long project_id;
+
+    private int up;
+
+    private int down;
+
+    public CommentDTO(@NonNull Comment comment) {
+        this.id = comment.getId();
+        this.nick = comment.getNick();
+        this.message = comment.getMessage();
+        this.project_id = comment.getProject().getId();
+        this.up = comment.getUp();
+        this.down = comment.getDown();
+    }
+}
