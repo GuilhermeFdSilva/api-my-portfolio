43322c300a35085939eabc2febac81cf9e18f674
Guilherme <francaguilherme27@gmail.com>
2024-01-23 20:59:59 -0300

    Novo handler

--- src/main/java/br/com/francaguilherme/myportfolio/controllers/adviceController/ExceptionHandlers.java
+++ src/main/java/br/com/francaguilherme/myportfolio/controllers/adviceController/ExceptionHandlers.java
@@ -7,9 +7,13 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.List;
 import java.util.Set;
 
 @ControllerAdvice
@@ -42,5 +46,19 @@ public class ExceptionHandlers {
         });
 
         return new ResponseEntity<>(response.toString(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handlerMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+        BindingResult result = exception.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+
+        StringBuilder errorMessage = new StringBuilder("Campos necessarios: \n");
+
+        for (FieldError fieldError : fieldErrors) {
+            errorMessage.append(fieldError.getField()).append(": ").append(fieldError.getDefaultMessage()).append(";\n");
+        }
+
+        return new ResponseEntity<>(errorMessage.toString(), HttpStatus.BAD_REQUEST);
     }
 }
