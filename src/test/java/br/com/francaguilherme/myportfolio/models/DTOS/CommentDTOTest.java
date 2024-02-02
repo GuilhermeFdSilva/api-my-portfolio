d35457a576fcc25fae01189cbd697d8d25eb7fe1
Guilherme <francaguilherme27@gmail.com>
2024-02-01 21:55:00 -0300

    Testes unitários

--- src/test/java/br/com/francaguilherme/myportfolio/models/DTOS/CommentDTOTest.java
+++ src/test/java/br/com/francaguilherme/myportfolio/models/DTOS/CommentDTOTest.java
@@ -1,7 +1,126 @@
 package br.com.francaguilherme.myportfolio.models.DTOS;
 
+import br.com.francaguilherme.myportfolio.models.DTOs.CommentDTO;
+import br.com.francaguilherme.myportfolio.models.entities.Comment;
+import br.com.francaguilherme.myportfolio.models.entities.Project;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class CommentDTOTest {
+    private CommentDTO commentDTO;
+
+    @BeforeEach
+    void setUp() {
+        commentDTO = new CommentDTO();
+    }
+
+    @Test
+    void testSetAndGetId(){
+        assertNull(commentDTO.getId());
+
+        commentDTO.setId(1L);
+
+        assertEquals(1L, commentDTO.getId());
+    }
+
+    @Test
+    void testSetAndGetNick(){
+        assertNull(commentDTO.getNick());
+
+        commentDTO.setNick("nick");
+
+        assertEquals("nick", commentDTO.getNick());
+    }
+
+    @Test
+    void testSetAndGetMessage(){
+        assertNull(commentDTO.getMessage());
+
+        commentDTO.setMessage("message");
+
+        assertEquals("message", commentDTO.getMessage());
+    }
+
+    @Test
+    void testSetAndGetProject_id(){
+        assertNull(commentDTO.getProject_id());
+
+        commentDTO.setProject_id(1L);
+
+        assertEquals(1L, commentDTO.getProject_id());
+    }
+
+    @Test
+    void testSetAndGetUp(){
+        assertEquals(0, commentDTO.getUp());
+
+        commentDTO.setUp(10);
+
+        assertEquals(10, commentDTO.getUp());
+    }
+
+    @Test
+    void testSetAndGetDown(){
+        assertEquals(0, commentDTO.getDown());
+
+        commentDTO.setDown(10);
+
+        assertEquals(10, commentDTO.getDown());
+    }
+
+    @Test
+    void testToDTO() {
+        Project project = new Project();
+        project.setId(1L);
+
+        Comment commentEntity = new Comment();
+        commentEntity.setId(1L);
+        commentEntity.setNick("nick");
+        commentEntity.setMessage("message");
+        commentEntity.setProject(project);
+        commentEntity.setUp(10);
+        commentEntity.setDown(10);
+
+        commentDTO = CommentDTO.toDTO(commentEntity);
+
+        assertAll(
+                () -> assertEquals(commentEntity.getId(), commentDTO.getId()),
+                () -> assertEquals(commentEntity.getNick(), commentDTO.getNick()),
+                () -> assertEquals(commentEntity.getMessage(), commentDTO.getMessage()),
+                () -> assertEquals(commentEntity.getProject().getId(), commentDTO.getProject_id()),
+                () -> assertEquals(commentEntity.getUp(), commentDTO.getUp()),
+                () -> assertEquals(commentEntity.getDown(), commentDTO.getDown())
+        );
+    }
+
+    @Test
+    void testListToDTO() {
+        List<Comment> comments = new ArrayList<>();
+        List<CommentDTO> commentDTOs;
+
+        for (long i = 0; i < 3; i++) {
+            Comment comment = new Comment();
+            comment.setId(i);
+            comment.setProject(new Project());
+
+            comments.add(comment);
+        }
+
+        commentDTOs = CommentDTO.listToDTO(comments);
+
+        assertAll(
+                () -> {
+                    for (int i = 0; i < commentDTOs.size(); i++) {
+                        assertEquals(comments.get(i).getId(), commentDTOs.get(i).getId());
+                    }
+                }
+        );
+    }
 }
--- src/test/java/br/com/francaguilherme/myportfolio/models/DTOS/ProjectDTOTest.java
+++ src/test/java/br/com/francaguilherme/myportfolio/models/DTOS/ProjectDTOTest.java
@@ -1,7 +1,173 @@
 package br.com.francaguilherme.myportfolio.models.DTOS;
 
+import br.com.francaguilherme.myportfolio.models.DTOs.ProjectDTO;
+import br.com.francaguilherme.myportfolio.models.entities.Language;
+import br.com.francaguilherme.myportfolio.models.entities.Project;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class ProjectDTOTest {
+    private ProjectDTO projectDTO;
+
+    private final byte[] README = new byte[]{};
+    private final String[] TOOLS = new String[]{};
+
+    @BeforeEach
+    void setUp() {
+        projectDTO = new ProjectDTO();
+    }
+
+    @Test
+    void setAndGetId() {
+        assertNull(projectDTO.getId());
+
+        projectDTO.setId(1L);
+
+        assertEquals(1L, projectDTO.getId());
+    }
+
+    @Test
+    void setAndGetTitle() {
+        assertNull(projectDTO.getTitle());
+
+        projectDTO.setTitle("title");
+
+        assertEquals("title", projectDTO.getTitle());
+    }
+
+    @Test
+    void setAndGetImage() {
+        assertNull(projectDTO.getImage());
+
+        projectDTO.setImage("image");
+
+        assertEquals("image", projectDTO.getImage());
+    }
+
+    @Test
+    void setAndGetDescription() {
+        assertNull(projectDTO.getDescription());
+
+        projectDTO.setDescription("desc");
+
+        assertEquals("desc", projectDTO.getDescription());
+    }
+
+    @Test
+    void setAndGetMainLanguageId() {
+        assertNull(projectDTO.getMain_language_id());
+
+        projectDTO.setMain_language_id(1L);
+
+        assertEquals(1L, projectDTO.getMain_language_id());
+    }
+
+    @Test
+    void setAndGetReadme() {
+        assertNull(projectDTO.getReadme());
+
+        projectDTO.setReadme(README);
+
+        assertEquals(README, projectDTO.getReadme());
+    }
+
+    @Test
+    void setAndGetLink_gh() {
+        assertNull(projectDTO.getLink_gh());
+
+        projectDTO.setLink_gh("link");
+
+        assertEquals("link", projectDTO.getLink_gh());
+    }
+
+    @Test
+    void setAndGetLink_pg() {
+        assertNull(projectDTO.getLink_pg());
+
+        projectDTO.setLink_pg("link");
+
+        assertEquals("link", projectDTO.getLink_pg());
+    }
+
+    @Test
+    void setAndGetTools() {
+        assertNull(projectDTO.getTools());
+
+        projectDTO.setTools(TOOLS);
+
+        assertEquals(TOOLS, projectDTO.getTools());
+    }
+
+    @Test
+    void setAndGetLikes() {
+        assertEquals(0, projectDTO.getLikes());
+
+        projectDTO.setLikes(10);
+
+        assertEquals(10, projectDTO.getLikes());
+    }
+
+    @Test
+    void testToDTO() {
+        Language language = new Language();
+        language.setId(1L);
+
+        Project projectEntity = new Project();
+        projectEntity.setId(1L);
+        projectEntity.setTitle("title");
+        projectEntity.setImage("image");
+        projectEntity.setDescription("desc");
+        projectEntity.setMain_language(language);
+        projectEntity.setReadme(README);
+        projectEntity.setLink_gh("gh");
+        projectEntity.setLink_pg("pg");
+        projectEntity.setTools(TOOLS);
+        projectEntity.setLikes(10);
+
+        projectDTO = ProjectDTO.toDTO(projectEntity);
+
+        assertAll(
+                () -> assertEquals(projectEntity.getId(), projectDTO.getId()),
+                () -> assertEquals(projectEntity.getTitle(), projectDTO.getTitle()),
+                () -> assertEquals(projectEntity.getImage(), projectDTO.getImage()),
+                () -> assertEquals(projectEntity.getDescription(), projectDTO.getDescription()),
+                () -> assertEquals(projectEntity.getMain_language().getId(), projectDTO.getMain_language_id()),
+                () -> assertEquals(projectEntity.getReadme(), projectDTO.getReadme()),
+                () -> assertEquals(projectEntity.getLink_gh(), projectDTO.getLink_gh()),
+                () -> assertEquals(projectEntity.getLink_pg(), projectDTO.getLink_pg()),
+                () -> assertEquals(projectEntity.getTools(), projectDTO.getTools()),
+                () -> assertEquals(projectEntity.getLikes(), projectDTO.getLikes())
+        );
+    }
+
+    @Test
+    void testListToDTO() {
+        List<Project> projects = new ArrayList<>();
+        List<ProjectDTO> projectDTOs;
+
+        for (long i = 0; i < 3; i++) {
+            Project project = new Project();
+            project.setId(i);
+            project.setMain_language(new Language());
+
+            projects.add(project);
+        }
+
+        projectDTOs = ProjectDTO.listToDTO(projects);
+
+        assertAll(
+                () -> {
+                    for (int i = 0; i < projectDTOs.size(); i++) {
+                        assertEquals(projects.get(i).getId(), projectDTOs.get(i).getId());
+                    }
+                }
+        );
+    }
 }
