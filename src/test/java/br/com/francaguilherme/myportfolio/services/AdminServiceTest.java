e381db6438bb18d5ae0a2df1c60743c831455a92
Guilherme <francaguilherme27@gmail.com>
2024-02-03 22:44:50 -0300

    Ajustes de testes

--- src/test/java/br/com/francaguilherme/myportfolio/services/AdminServiceTest.java
+++ src/test/java/br/com/francaguilherme/myportfolio/services/AdminServiceTest.java
@@ -10,7 +10,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
